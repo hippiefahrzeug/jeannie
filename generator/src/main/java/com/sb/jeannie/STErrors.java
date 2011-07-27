bfc6c1d8049bfcb02cc5d5152a06392d498a9e00
alvi <git@o08.net>
2011-07-27 16:54:12 +0200

    removed unused class

--- generator/src/main/java/com/sb/jeannie/STErrors.java
+++ generator/src/main/java/com/sb/jeannie/STErrors.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.stringtemplate.v4.STErrorListener;
-import org.stringtemplate.v4.misc.Misc;
 import org.stringtemplate.v4.misc.STMessage;
 import org.stringtemplate.v4.misc.STNoSuchPropertyException;
 
